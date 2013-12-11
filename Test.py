389efe8a60ece71802c9d7971f346ac77bad6e74
unknown <mvyas@.snstech.com>
2013-12-11 21:15:00 +0530

    fg

--- Test.py
+++ Test.py
@@ -1,33 +1,22 @@
-print "Hello this is the output of simple program."
-
-
-# 
-# import requests
-# import csv
-# import os,sys
-# import robot
-# from requests.packages.urllib3.poolmanager import SSL_KEYWORDS
-# from requests.exceptions import SSLError
-# import urllib
-# import httplib
-# from requests.packages.urllib3.connectionpool import HTTPSConnection
-# from requests.api import get, request
-# from robot import rebot
-# s=requests.session()
-# 
-# url='https://snapshot.cloud-elements.com/elements/api-v1/marketing/retrieveCampaign?elementToken=5044bcae98bd59c96d20b2d92eff780f&campaignId=132'
-# headers="application/json"
-# 
-# proxies = {'https': 'http://10.1.2.3:8080'}
-# 
-# #CA_CERTS = os.path.join(os.path.dirname(sys.executable), "")
-# r1 = requests.auth.HTTPBasicAuth("madhusudan.vyas@snstech.com","cloud!23") 
-# #url='https://snapshot.cloud-elements.com/elements/api-v1/marketing/retrieveCampaign?elementToken=5044bcae98bd59c96d20b2d92eff780f&campaignId=122'
-# headers="Content-type: application/json"
-# 
-# r=get(url,auth=r1)
-# status = r.status_code
-# responsedata = r.text
-# print str(r.text)
-
-
+import requests,sys
+import json
+from compiler.ast import List
+from fileresponse import fileresponse 
+class Test15_DelCampaign():
+    
+        
+    name=Test1
+    url1="http://google.com"    
+    
+    #r1 = requests.auth.HTTPBasicAuth("madhusudan.vyas@snstech.com","cloud!23") 
+    r = requests.get(url1)
+    status = r.status_code
+    responsedata = r.text
+    print str(r.text)   
+    responsedata = r.text       
+    print ("Testcase passed")    
+    datafile=List  
+    datafile=[responsedata,status]     
+    type(fileresponse)     
+    fr=fileresponse()
+    filewrite= fr.fileresp(datafile)
